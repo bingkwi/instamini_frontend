63bcf33267e2f91edbfe1d98578c1bfa44f8e034
Bích Quỳnh <duongquynh0617@gmail.com>
2020-06-24 17:13:55 +0700

    quynhndb: add post input validation

--- src/components/PostInput/index.js
+++ src/components/PostInput/index.js
@@ -6,11 +6,20 @@ class PostInput extends Component {
         super(props);
         this.state = {
             caption: "",
-            files: undefined
+            files: undefined,
+            canSubmit: false
         };
+        this.fileInput = undefined;
     }
 
-    handleSubmitCallback = () => this.props.handleSubmit(this.state.caption, this.state.files)
+    handleSubmitCallback = () => {
+        this.props.handleSubmit(this.state.caption, this.state.files);
+        this.setState({
+            caption: "",
+            files: undefined,
+            canSubmit: false
+        }, () => this.fileInput.value = null);
+    }
 
     render() {
         return (
@@ -22,12 +31,12 @@ class PostInput extends Component {
                     <img src="./images/test.jpg" className="img-fluid rounded-circle mr-2" style={{ objectFit: "cover", width: "35px", height: "35px" }} alt="" />
                     <textarea type="text" className="form-control border border-white card container" 
                         placeholder="What's on your mind?" style={{ resize: "none" }} value={this.state.caption}
-                        onChange={e => this.setState({ caption: e.target.value })} />
-                    <input type="file" multiple className="form-control-file" 
-                        onChange={e => this.setState({ files: e.target.files })} />
+                        onChange={e => this.setState({ caption: e.target.value }, () => { if (this.state.caption !== "") { this.setState({ canSubmit: false }); } else { this.setState({ canSubmit: false }); } })} />
+                    <input type="file" multiple className="form-control-file" ref={btn => this.fileInput = btn}
+                        onChange={e => this.setState({ files: e.target.files }, () => { if (this.state.files) { this.setState({ canSubmit: true }); } else { this.setState({ canSubmit: true }); } })} />
                 </div>
 
-                <button type="button" className="btn btn-primary btn-md btn-block py-1" onClick={this.handleSubmitCallback}>Post</button>
+                <button type="button" className="btn btn-primary btn-md btn-block py-1" onClick={this.handleSubmitCallback} disabled={!this.state.canSubmit}>Post</button>
 
             </div>
         );
